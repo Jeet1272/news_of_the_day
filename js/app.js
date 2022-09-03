7c297c7d87e295fc27d9923e61c6dcc5ff95a9e0
Jeet1272 <bishwajeetsarkarbd@gmail.com>
2022-09-04 02:02:13 +0600

    final commit

--- js/app.js
+++ js/app.js
@@ -98,3 +98,14 @@ const modalViewrDisply = (category) => {
 }
 
 loadCategories()
+/* ----------------------Blog Section -------------------- */
+const blogDisply = () => {
+    const displyElement = document.getElementById('blog')
+    displyElement.innerHTML = `
+    <p>1.var variables can be re-declared and updated, var variables are hoisted to the top of their scope and initialized with a value of undefined. let is block scoped and let can be updated but not re-declared. const cannot be updated or re-declared</p>
+    <p>2.arrow functions do not have their own this but regular function  have. Inside the body of a regular function, arguments is a special array-like object containing the list of arguments with which the function has been invoked. On the other side, no arguments special keyword is defined inside an arrow function.</p>
+    <p>3. .map() executes the same code on every element in an array and returns a new array with the updated elements. .forEach(), is used to execute the same code on every element in an array but does not change the array and it returns undefined. .filter() checks every element in an array to see if it meets a certain criteria and returns a new array with the elements that return truthy for the criteria. The .find() method returns the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned.</p>
+    <p>4.Template literals provide an easy way to interpolate variables and expressions into strings. The method is called string interpolation.</p>
+    
+    `
+}
